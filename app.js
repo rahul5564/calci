07c4b2ab866afe1ea21323978899b123f10aa5ac
rahul <sraghavendra5564@gmail.com>
2021-05-10 18:08:50 +0530

    .

---
+++ app.js
@@ -0,0 +1,8 @@
+var btn1 = document.getElementsByClassName('button1').addEventListener('click',foo());
+
+var inputText = document.getElementsByClassName('input-textpppppppppp');
+
+function foo(){
+	var btvalue = "1" ;
+	inputText.innerHTML = btvalue;
+}
